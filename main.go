d0dbc85c9de564258e626e663928e10e4778bc6f
hujing <85513490@qq.com>
2024-09-12 20:29:49 +0800

    添加网络监听

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello, Go!")
+}
 
 func main() {
-    fmt.Println("Hello, Go!")
+    http.HandleFunc("/", handler)
+    fmt.Println("Listening on :9090")
+    err := http.ListenAndServe(":9090", nil)
+    if err != nil {
+        fmt.Println("Error starting server:", err)
+    }
 }
