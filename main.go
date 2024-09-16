168f9665323d7112bb0f929055224418e0f5ff83
hujing <85513490@qq.com>
2024-09-16 11:59:19 +0800

    修改端口

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, Go!")
+    fmt.Fprintf(w, "Hello, Go 副本信息!")
 }
 
 func main() {
     http.HandleFunc("/", handler)
-    fmt.Println("Listening on port 9090...")
-    err := http.ListenAndServe(":9090", nil)
+    fmt.Println("Listening on port 9091...")
+    err := http.ListenAndServe(":9091", nil)
     if err != nil {
         fmt.Println("Error starting server:", err)
     }
